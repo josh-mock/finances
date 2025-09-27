0f6f4b70487ea11148ba041248b250f440fbff58
jwmock88 <jwmock@proton.me>
2025-09-27 15:07:40 +0100

    acounts routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import bankRoutes from "./routes/banks.js";
+import accountRoutes from "./routes/accounts.js";
 dotenv.config();
 
 const port = 3000;
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/banks", bankRoutes);
+app.use("/accounts", accountRoutes);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
