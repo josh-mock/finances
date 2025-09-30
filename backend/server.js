e0396a2855f77ecec7509229bd395407a93fb000
jwmock88 <jwmock@proton.me>
2025-09-30 23:26:20 +0100

    category routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import bankRoutes from "./routes/banks.js";
 import accountRoutes from "./routes/accounts.js";
+import categoryRoutes from "./routes/categories.js";
 dotenv.config();
 
 const port = 3000;
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/banks", bankRoutes);
 app.use("/accounts", accountRoutes);
+app.use("/categories", categoryRoutes);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
