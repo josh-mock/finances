5c0dcdca3aa627491004bb1474bb5595456fc502
jwmock88 <jwmock@proton.me>
2025-10-03 16:40:03 +0100

    moved components to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import Admin from "./components/admin/admin";
+import Admin from "./pages/admin/admin";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Layout from "./components/layout/layout";
 export default function App() {
   return (
