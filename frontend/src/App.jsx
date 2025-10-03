9d7a79def98b3ae9f5881284d277a4175e6edd9a
jwmock88 <jwmock@proton.me>
2025-10-03 16:41:45 +0100

    Transactions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Admin from "./pages/admin/admin";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/layout/layout";
+import Transactions from "./pages/transactions/Transactions";
 export default function App() {
   return (
     <Router>
@@ -9,6 +10,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="/transactions" element={<Transactions />} />
         </Route>
       </Routes>
     </Router>
