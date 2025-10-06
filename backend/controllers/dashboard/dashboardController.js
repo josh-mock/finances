344ec2000d9ed10200583f6b25bb324067f04759
jwmock88 <jwmock@proton.me>
2025-10-06 17:02:09 +0100

    Balances table

--- backend/controllers/dashboard/dashboardController.js
+++ backend/controllers/dashboard/dashboardController.js
@@ -3,21 +3,35 @@ import pool from "../../db/db.js";
 export const getBalances = async (req, res) => {
   try {
     const result = await pool.query(`
-      SELECT
-          a.id AS account_id,
-          a.account_name,
-          COALESCE(SUM(
-              CASE 
-                  WHEN t.type IN ('Expense', 'Transfer Out') THEN -t.amount
-                  ELSE t.amount
-              END
-          ), 0) AS balance,
-          MAX(t.date) AS last_transaction
-      FROM accounts a
-      LEFT JOIN transactions t ON a.id = t.account_id
-      WHERE a.is_closed = FALSE
-      GROUP BY a.id, a.account_name
-      ORDER BY a.id;
+WITH total AS (
+    SELECT COALESCE(SUM(
+        CASE 
+            WHEN type IN ('Expense', 'Transfer Out') THEN -amount
+            ELSE amount
+        END
+    ), 0) AS total_balance
+    FROM transactions t
+    JOIN accounts a ON t.account_id = a.id
+    WHERE a.is_closed = FALSE
+)
+SELECT
+    a.id AS account_id,
+    a.account_name,
+    COALESCE(SUM(
+        CASE 
+            WHEN t.type IN ('Expense', 'Transfer Out') THEN -t.amount
+            ELSE t.amount
+        END
+    ), 0) AS balance,
+    MAX(t.date) AS last_transaction,
+    total.total_balance
+FROM accounts a
+LEFT JOIN transactions t ON a.id = t.account_id
+CROSS JOIN total
+WHERE a.is_closed = FALSE
+GROUP BY a.id, a.account_name, total.total_balance
+ORDER BY a.id;
+
     `);
     res.json(result.rows);
   } catch (err) {
--- frontend/src/pages/dashboard/balances/BalancesTable.jsx
+++ frontend/src/pages/dashboard/balances/BalancesTable.jsx
@@ -8,35 +8,53 @@ export default function BalancesTable() {
     queryFn: fetchBalances,
   });
 
+  const { total_balance } = balances[0];
+  const accounts = balances
+    .map(({ total_balance, ...rest }) => rest)
+    .sort((a, b) => a.account_name.localeCompare(b.account_name));
+
+  if (!balances.length)
+    return <div className="table__empty">No data.</div>;
+
   return (
     <div>
       <table className="table">
         <thead>
           <tr>
             <th>Account Name</th>
-            <th>Balance</th>
             <th>Last Transaction</th>
+            <th>Balance</th>
           </tr>
         </thead>
         <tbody>
-          {balances
-            .sort((a, b) => a.account_name.localeCompare(b.account_name))
-            .map((balance) => (
-              <tr key={balance.account_id}>
-                <td>{balance.account_name}</td>
-                <td>
-                  <div className="currency">
-                    <span className="currency__symbol">£</span>
-                    <span className="currency__value">
-                      {formatCurrency(balance.balance)}
-                    </span>
-                  </div>
-                </td>
-                <td>
-                  {new Date(balance.last_transaction).toLocaleDateString()}
-                </td>
-              </tr>
-            ))}
+          {accounts.map((balance) => (
+            <tr key={balance.account_id}>
+              <td>{balance.account_name}</td>
+              <td>{new Date(balance.last_transaction).toLocaleDateString()}</td>
+
+              <td>
+                <div className="currency">
+                  <span className="currency__symbol">£</span>
+                  <span className="currency__value">
+                    {formatCurrency(balance.balance)}
+                  </span>
+                </div>
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td colSpan={2}>
+              <strong>Total</strong>
+            </td>
+            <td>
+              <div className="currency">
+                <span className="currency__symbol">£</span>
+                <strong className="currency__value">
+                  {formatCurrency(total_balance)}
+                </strong>
+              </div>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
