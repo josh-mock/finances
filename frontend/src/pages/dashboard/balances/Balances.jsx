a3eafe95c9f162aea735912122214daecf3c5f82
jwmock88 <jwmock@proton.me>
2025-10-07 13:19:10 +0100

    edits

--- frontend/src/pages/dashboard/balances/Balances.jsx
+++ frontend/src/pages/dashboard/balances/Balances.jsx
@@ -5,10 +5,8 @@ export default function Balances() {
   return (
     <>
       <h3>Balances</h3>
-      <div>
-        <BalancesTable />
-        <BalancesGraph />
-      </div>
+      <BalancesTable />
+      <BalancesGraph />
     </>
   );
 }
